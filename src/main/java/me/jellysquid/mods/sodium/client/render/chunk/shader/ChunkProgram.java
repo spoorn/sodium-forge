ba10221cfbcdb69816dcc72cbbb789ba3eb68978
TwistedZero <51268715+TwistedZero@users.noreply.github.com>
2021-02-15 02:39:20 +0000

    fix: Reduce inaccuracies in vertex positions and texture coordinates with the Compact Vertex Format

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkProgram.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkProgram.java
@@ -1,11 +1,11 @@
 package me.jellysquid.mods.sodium.client.render.chunk.shader;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.gl.shader.GlProgram;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Matrix4f;
 import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL20C;
 import org.lwjgl.system.MemoryStack;
 
 import java.nio.FloatBuffer;
@@ -21,6 +21,10 @@ public abstract class ChunkProgram extends GlProgram {
     protected static final float MODEL_SIZE = 32.0f;
 
     protected static final float TEXTURE_SIZE = 1.0f;
+
+    protected static final float CVF_MODEL_SIZE = (32.0f / 65536.0f);
+
+    protected static final float CVF_TEXTURE_SIZE = (1.0f / 32768.0f);
 
     // Uniform variable binding indexes
     private final int uModelViewProjectionMatrix;
@@ -48,8 +52,13 @@ public abstract class ChunkProgram extends GlProgram {
         GL20.glUniform1i(this.uBlockTex, 0);
         GL20.glUniform1i(this.uLightTex, 2);
 
-        GL20.glUniform3f(this.uModelScale, MODEL_SIZE, MODEL_SIZE, MODEL_SIZE);
-        GL20C.glUniform2f(this.uTextureScale, TEXTURE_SIZE, TEXTURE_SIZE);
+        if (SodiumClientMod.options().advanced.useCompactVertexFormat) {
+            GL20.glUniform3f(this.uModelScale, CVF_MODEL_SIZE, CVF_MODEL_SIZE, CVF_MODEL_SIZE);
+            GL20.glUniform2f(this.uTextureScale, CVF_TEXTURE_SIZE, CVF_TEXTURE_SIZE);
+        } else {
+            GL20.glUniform3f(this.uModelScale, MODEL_SIZE, MODEL_SIZE, MODEL_SIZE);
+            GL20.glUniform2f(this.uTextureScale, TEXTURE_SIZE, TEXTURE_SIZE);
+        }
 
         this.fogShader.setup();
 
