460d8a13253ae13eefbce6fa2e0fd71b39906fd4
spoorn <michael.trinh1996@gmail.com>
2021-05-31 22:33:30 -0700

    Do not require Sodium on server side, server causes errors

--- src/main/java/me/jellysquid/mods/sodium/client/SodiumClientMod.java
+++ src/main/java/me/jellysquid/mods/sodium/client/SodiumClientMod.java
@@ -2,7 +2,11 @@ package me.jellysquid.mods.sodium.client;
 
 import me.jellysquid.mods.sodium.client.gui.SodiumGameOptions;
 import me.jellysquid.mods.sodium.client.util.UnsafeUtil;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.ModList;
@@ -18,6 +22,9 @@ public class SodiumClientMod {
         ftbChunksLoaded = ModList.get().isLoaded("ftbchunks");
         // Register the setup method for modloading
         //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
     //public void setup(final FMLCommonSetupEvent event) {
     //
