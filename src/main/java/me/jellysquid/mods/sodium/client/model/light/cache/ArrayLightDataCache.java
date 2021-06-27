CaffeineMC/sodium-fabric

change: Split block data arrays into sections for world slicing
This allows much simpler block copy logic to be used during setup, which
helps the render thread get through rebuild tasks sooner.

Additionally, it allows optional direct access into the origin section's
block data array to speed up iteration while rebuilding.

Reading blocks from the arrays is slightly faster as indices can be computed
using simpler bit shifts and compile-time constants.

src/main/java/me/jellysquid/mods/sodium/client/model/light/cache/ArrayLightDataCache.java
@@ -1,6 +1,7 @@
 package me.jellysquid.mods.sodium.client.model.light.cache;
 
 import me.jellysquid.mods.sodium.client.model.light.data.LightDataAccess;
+import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.world.BlockRenderView;
 
 import java.util.Arrays;
@@ -11,36 +12,33 @@ import java.util.Arrays;
  * can be re-used by {@link me.jellysquid.mods.sodium.client.world.WorldSlice} to avoid allocations.
  */
 public class ArrayLightDataCache extends LightDataAccess {
+    private static final int NEIGHBOR_BLOCK_RADIUS = 2;
+    private static final int BLOCK_LENGTH = 16 + (NEIGHBOR_BLOCK_RADIUS * 2);
+
     private final long[] light;
-    private final int xSize, ySize, zSize;
+
     private int xOffset, yOffset, zOffset;
 
-    public ArrayLightDataCache(int size) {
-        this(size, size, size);
+    public ArrayLightDataCache() {
+        this.light = new long[BLOCK_LENGTH * BLOCK_LENGTH * BLOCK_LENGTH];
     }
 
-    public ArrayLightDataCache(int xSize, int ySize, int zSize) {
-        this.xSize = xSize;
-        this.ySize = ySize;
-        this.zSize = zSize;
-
-        int len = xSize * ySize * zSize;
-
-        this.light = new long[len];
-    }
-
-    public void init(BlockRenderView world, int x, int y, int z) {
+    public void init(BlockRenderView world, ChunkSectionPos origin) {
         this.world = world;
-        this.xOffset = x;
-        this.yOffset = y;
-        this.zOffset = z;
+
+        this.xOffset = origin.getMinX() - NEIGHBOR_BLOCK_RADIUS;
+        this.yOffset = origin.getMinY() - NEIGHBOR_BLOCK_RADIUS;
+        this.zOffset = origin.getMinZ() - NEIGHBOR_BLOCK_RADIUS;
 
         Arrays.fill(this.light, 0L);
     }
 
     private int index(int x, int y, int z) {
-        // TODO: simplify
-        return (z - this.zOffset) * this.xSize * this.ySize + (y - this.yOffset) * this.zSize + x - this.xOffset;
+        int x2 = x - this.xOffset;
+        int y2 = y - this.yOffset;
+        int z2 = z - this.zOffset;
+
+        return (z2 * BLOCK_LENGTH * BLOCK_LENGTH) + (y2 * BLOCK_LENGTH) + x2;
     }
 
     @Override

src/main/java/me/jellysquid/mods/sodium/client/render/chunk/tasks/ChunkRenderRebuildTask.java
@@ -54,35 +54,34 @@ public class ChunkRenderRebuildTask<T extends ChunkGraphicsState> extends ChunkR
         ChunkOcclusionDataBuilder occluder = new ChunkOcclusionDataBuilder();
         ChunkRenderBounds.Builder bounds = new ChunkRenderBounds.Builder();
 
-        pipeline.init(this.slice, this.slice.getBlockOffsetX(), this.slice.getBlockOffsetY(), this.slice.getBlockOffsetZ());
+        pipeline.init(this.slice, this.slice.getOrigin());
         buffers.init(renderData);
 
-        int minX = this.render.getOriginX();
-        int minY = this.render.getOriginY();
-        int minZ = this.render.getOriginZ();
-
-        int maxX = minX + 16;
-        int maxY = minY + 16;
-        int maxZ = minZ + 16;
+        int baseX = this.render.getOriginX();
+        int baseY = this.render.getOriginY();
+        int baseZ = this.render.getOriginZ();
 
         BlockPos.Mutable pos = new BlockPos.Mutable();
         BlockPos offset = this.offset;
 
-        for (int y = minY; y < maxY; y++) {
+        for (int relY = 0; relY < 16; relY++) {
             if (cancellationSource.isCancelled()) {
                 return null;
             }
 
-            for (int z = minZ; z < maxZ; z++) {
-                for (int x = minX; x < maxX; x++) {
-                    BlockState blockState = this.slice.getBlockState(x, y, z);
-                    Block block = blockState.getBlock();
+            for (int relZ = 0; relZ < 16; relZ++) {
+                for (int relX = 0; relX < 16; relX++) {
+                    BlockState blockState = this.slice.getOriginBlockState(relX, relY, relZ);
 
                     if (blockState.isAir()) {
                         continue;
                     }
 
-                    pos.set(x, y, z);
+                    Block block = blockState.getBlock();
+
+                    int x = baseX + relX;
+                    int y = baseY + relY;
+                    int z = baseZ + relZ;
 
                     if (block.getRenderType(blockState) == BlockRenderType.MODEL) {
                         RenderLayer layer = RenderLayers.getBlockLayer(blockState);
@@ -90,7 +89,7 @@ public class ChunkRenderRebuildTask<T extends ChunkGraphicsState> extends ChunkR
                         ChunkBuildBuffers.ChunkBuildBufferDelegate builder = buffers.get(layer);
                         builder.setOffset(x - offset.getX(), y - offset.getY(), z - offset.getZ());
 
-                        if (pipeline.renderBlock(this.slice, blockState, pos, builder, true)) {
+                        if (pipeline.renderBlock(this.slice, blockState, pos.set(x, y, z), builder, true)) {
                             bounds.addBlock(x, y, z);
                         }
                     }
@@ -103,13 +102,13 @@ public class ChunkRenderRebuildTask<T extends ChunkGraphicsState> extends ChunkR
                         ChunkBuildBuffers.ChunkBuildBufferDelegate builder = buffers.get(layer);
                         builder.setOffset(x - offset.getX(), y - offset.getY(), z - offset.getZ());
 
-                        if (pipeline.renderFluid(this.slice, fluidState, pos, builder)) {
+                        if (pipeline.renderFluid(this.slice, fluidState, pos.set(x, y, z), builder)) {
                             bounds.addBlock(x, y, z);
                         }
                     }
 
                     if (block.hasBlockEntity()) {
-                        BlockEntity entity = this.slice.getBlockEntity(pos, WorldChunk.CreationType.CHECK);
+                        BlockEntity entity = this.slice.getBlockEntity(pos.set(x, y, z), WorldChunk.CreationType.CHECK);
 
                         if (entity != null) {
                             BlockEntityRenderer<BlockEntity> renderer = BlockEntityRenderDispatcher.INSTANCE.get(entity);

src/main/java/me/jellysquid/mods/sodium/client/render/pipeline/context/ChunkRenderContext.java
@@ -7,13 +7,13 @@ import me.jellysquid.mods.sodium.client.model.quad.sink.ModelQuadSinkDelegate;
 import me.jellysquid.mods.sodium.client.render.pipeline.BlockRenderer;
 import me.jellysquid.mods.sodium.client.render.pipeline.FluidRenderer;
 import me.jellysquid.mods.sodium.client.render.pipeline.RenderContextCommon;
-import me.jellysquid.mods.sodium.client.world.WorldSlice;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.block.BlockModels;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.world.BlockRenderView;
 
 public class ChunkRenderContext {
@@ -25,7 +25,7 @@ public class ChunkRenderContext {
     private final BlockModels models;
 
     public ChunkRenderContext(MinecraftClient client) {
-        this.lightDataCache = new ArrayLightDataCache(WorldSlice.BLOCK_LENGTH);
+        this.lightDataCache = new ArrayLightDataCache();
 
         LightPipelineProvider lightPipelineProvider = new LightPipelineProvider(this.lightDataCache);
         BiomeColorBlender biomeColorBlender = RenderContextCommon.createBiomeColorBlender();
@@ -47,7 +47,7 @@ public class ChunkRenderContext {
         return this.fluidRenderer.render(world, fluidState, pos, consumer);
     }
 
-    public void init(BlockRenderView world, int x, int y, int z) {
-        this.lightDataCache.init(world, x, y, z);
+    public void init(BlockRenderView world, ChunkSectionPos pos) {
+        this.lightDataCache.init(world, pos);
     }
 }

spoorn/sodium-forge

src/main/java/me/jellysquid/mods/sodium/client/model/light/cache/ArrayLightDataCache.java
@@ -1,6 +1,7 @@
 package me.jellysquid.mods.sodium.client.model.light.cache;
 
 import me.jellysquid.mods.sodium.client.model.light.data.LightDataAccess;
+import net.minecraft.util.math.SectionPos;
 import net.minecraft.world.IBlockDisplayReader;
 
 import java.util.Arrays;
@@ -11,36 +12,33 @@ import java.util.Arrays;
  * can be re-used by {@link me.jellysquid.mods.sodium.client.world.WorldSlice} to avoid allocations.
  */
 public class ArrayLightDataCache extends LightDataAccess {
+    private static final int NEIGHBOR_BLOCK_RADIUS = 2;
+    private static final int BLOCK_LENGTH = 16 + (NEIGHBOR_BLOCK_RADIUS * 2);
+
     private final long[] light;
-    private final int xSize, ySize, zSize;
+
     private int xOffset, yOffset, zOffset;
 
-    public ArrayLightDataCache(int size) {
-        this(size, size, size);
+    public ArrayLightDataCache() {
+        this.light = new long[BLOCK_LENGTH * BLOCK_LENGTH * BLOCK_LENGTH];
     }
 
-    public ArrayLightDataCache(int xSize, int ySize, int zSize) {
-        this.xSize = xSize;
-        this.ySize = ySize;
-        this.zSize = zSize;
-
-        int len = xSize * ySize * zSize;
-
-        this.light = new long[len];
-    }
-
-    public void init(IBlockDisplayReader world, int x, int y, int z) {
+    public void init(IBlockDisplayReader world, SectionPos origin) {
         this.world = world;
-        this.xOffset = x;
-        this.yOffset = y;
-        this.zOffset = z;
+
+        this.xOffset = origin.getWorldStartX() - NEIGHBOR_BLOCK_RADIUS;
+        this.yOffset = origin.getWorldStartY() - NEIGHBOR_BLOCK_RADIUS;
+        this.zOffset = origin.getWorldStartZ() - NEIGHBOR_BLOCK_RADIUS;
 
         Arrays.fill(this.light, 0L);
     }
 
     private int index(int x, int y, int z) {
-        // TODO: simplify
-        return (z - this.zOffset) * this.xSize * this.ySize + (y - this.yOffset) * this.zSize + x - this.xOffset;
+        int x2 = x - this.xOffset;
+        int y2 = y - this.yOffset;
+        int z2 = z - this.zOffset;
+
+        return (z2 * BLOCK_LENGTH * BLOCK_LENGTH) + (y2 * BLOCK_LENGTH) + x2;
     }
 
     @Override

src/main/java/me/jellysquid/mods/sodium/client/render/chunk/tasks/ChunkRenderRebuildTask.java
@@ -67,12 +67,12 @@ public class ChunkRenderRebuildTask<T extends ChunkGraphicsState> extends ChunkR
         VisGraph occluder = new VisGraph();
         ChunkRenderBounds.Builder bounds = new ChunkRenderBounds.Builder();
 
-        pipeline.init(this.slice, this.slice.getBlockOffsetX(), this.slice.getBlockOffsetY(), this.slice.getBlockOffsetZ());
+        pipeline.init(this.slice, this.slice.getOrigin());
         buffers.init(renderData);
 
-        int minX = this.render.getOriginX();
-        int minY = this.render.getOriginY();
-        int minZ = this.render.getOriginZ();
+        int baseX = this.render.getOriginX();
+        int baseY = this.render.getOriginY();
+        int baseZ = this.render.getOriginZ();
 
         BlockPos.Mutable pos = new BlockPos.Mutable();
         BlockPos offset = this.offset;
@@ -82,11 +82,11 @@ public class ChunkRenderRebuildTask<T extends ChunkGraphicsState> extends ChunkR
         // TODO: Since we're not sorting here anymore, no need to loop through coordinates twice
         BlockState[] blockStates = new BlockState[TOTAL_CHUNK_SIZE];
         Coordinate[] coordinates = new Coordinate[TOTAL_CHUNK_SIZE];
-        for (int y = minY; y < minY + CHUNK_BUILD_SIZE; y++) {
-            for (int z = minZ; z < minZ + CHUNK_BUILD_SIZE; z++) {
-                for (int x = minX; x < minX + CHUNK_BUILD_SIZE; x++) {
-                    BlockState curr = this.slice.getBlockState(x, y, z);
-                    int index = (x-minX)+((y-minY)*CHUNK_BUILD_SIZE)+((z-minZ)*CHUNK_BUILD_SIZE_2D);
+        for (int y = baseY; y < baseY + CHUNK_BUILD_SIZE; y++) {
+            for (int z = baseZ; z < baseZ + CHUNK_BUILD_SIZE; z++) {
+                for (int x = baseX; x < baseX + CHUNK_BUILD_SIZE; x++) {
+                    BlockState curr = this.slice.getOriginBlockState(x, y, z);
+                    int index = (x-baseX)+((y-baseY)*CHUNK_BUILD_SIZE)+((z-baseZ)*CHUNK_BUILD_SIZE_2D);
                     blockStates[index] = curr;
                     coordinates[index] = new Coordinate(x, y, z);
                     for (BlockRenderPass pass : BlockRenderPass.VALUES) {
@@ -115,7 +115,7 @@ public class ChunkRenderRebuildTask<T extends ChunkGraphicsState> extends ChunkR
                 return null;
             }
             setupBlockRender(pipeline, buffers, renderData, occluder, bounds, pos, offset, blockStates,
-                    coordinate.x, coordinate.y, coordinate.z, minX, minY, minZ);
+                    coordinate.x, coordinate.y, coordinate.z, baseX, baseY, baseZ);
         }
 
         for (BlockRenderPass pass : BlockRenderPass.VALUES) {

src/main/java/me/jellysquid/mods/sodium/client/render/pipeline/context/ChunkRenderContext.java
@@ -7,13 +7,13 @@ import me.jellysquid.mods.sodium.client.model.quad.sink.ModelQuadSinkDelegate;
 import me.jellysquid.mods.sodium.client.render.pipeline.BlockRenderer;
 import me.jellysquid.mods.sodium.client.render.pipeline.FluidRenderer;
 import me.jellysquid.mods.sodium.client.render.pipeline.RenderContextCommon;
-import me.jellysquid.mods.sodium.client.world.WorldSlice;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockModelShapes;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.SectionPos;
 import net.minecraft.world.IBlockDisplayReader;
 import net.minecraftforge.client.model.data.IModelData;
 
@@ -26,7 +26,7 @@ public class ChunkRenderContext {
     private final BlockModelShapes models;
 
     public ChunkRenderContext(Minecraft client) {
-        this.lightDataCache = new ArrayLightDataCache(WorldSlice.BLOCK_LENGTH);
+        this.lightDataCache = new ArrayLightDataCache();
 
         LightPipelineProvider lightPipelineProvider = new LightPipelineProvider(this.lightDataCache);
         BiomeColorBlender biomeColorBlender = RenderContextCommon.createBiomeColorBlender();
@@ -48,7 +48,7 @@ public class ChunkRenderContext {
         return this.fluidRenderer.render(world, fluidState, pos, consumer);
     }
 
-    public void init(IBlockDisplayReader world, int x, int y, int z) {
-        this.lightDataCache.init(world, x, y, z);
+    public void init(IBlockDisplayReader world, SectionPos pos) {
+        this.lightDataCache.init(world, pos);
     }
 }