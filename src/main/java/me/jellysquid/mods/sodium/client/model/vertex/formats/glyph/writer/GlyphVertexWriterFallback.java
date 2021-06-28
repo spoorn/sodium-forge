CaffeineMC/sodium-fabric

new: Implement optimized vertex writing
This system replaces the format-specific interfaces that had to
be implemented on a variety of VertexConsumer implementations.

Major speed ups are accomplished by hoisting as much validation
as possible out of the actual vertex writing steps. This allows
the CPU to streamline the process of writing data into memory
and significantly reduces the instruction count.

To write vertices, a specialized "sink" is now created on top
of a "drain" (such a VertexConsumer or BufferBuilder). When
trying to create a sink, the validation logic runs once to
make sure that it's valid to write vertices of the given format
into the drain.

Before writing vertices to a sink, the caller must first make a
call to ensure that there is necessary space in the sink for
the given number of vertices. This will make calls to resize
internal buffers if necessary to fit incoming data.

Once all vertices have been written to the sink, a final call
must be made to flush the vertices in the sink down the drain. If
the caller fails to flush, then the backing storage type will not
be notified that it needs to advance its write pointer or vertex
count, resulting in any previously written vertices being
overwritten. This behavior also enables "safe" zero-copy writing
as a failed write simply gets overwritten by the next caller safely.

src/main/java/me/jellysquid/mods/sodium/client/model/vertex/formats/glyph/writer/GlyphVertexWriterFallback.java
@@ -1,0 +1,22 @@
+package me.jellysquid.mods.sodium.client.model.vertex.formats.glyph.writer;
+
+import me.jellysquid.mods.sodium.client.model.vertex.fallback.VertexWriterFallback;
+import me.jellysquid.mods.sodium.client.model.vertex.formats.glyph.GlyphVertexSink;
+import me.jellysquid.mods.sodium.client.util.color.ColorABGR;
+import net.minecraft.client.render.VertexConsumer;
+
+public class GlyphVertexWriterFallback extends VertexWriterFallback implements GlyphVertexSink {
+    public GlyphVertexWriterFallback(VertexConsumer consumer) {
+        super(consumer);
+    }
+
+    @Override
+    public void writeGlyph(float x, float y, float z, int color, float u, float v, int light) {
+        VertexConsumer consumer = this.consumer;
+        consumer.vertex(x, y, z);
+        consumer.color(ColorABGR.unpackRed(color), ColorABGR.unpackGreen(color), ColorABGR.unpackBlue(color), ColorABGR.unpackAlpha(color));
+        consumer.texture(u, v);
+        consumer.light(light);
+        consumer.next();
+    }
+}

src/main/java/me/jellysquid/mods/sodium/client/model/vertex/formats/particle/writer/ParticleVertexWriterFallback.java
@@ -1,0 +1,22 @@
+package me.jellysquid.mods.sodium.client.model.vertex.formats.particle.writer;
+
+import me.jellysquid.mods.sodium.client.model.vertex.fallback.VertexWriterFallback;
+import me.jellysquid.mods.sodium.client.model.vertex.formats.particle.ParticleVertexSink;
+import me.jellysquid.mods.sodium.client.util.color.ColorABGR;
+import net.minecraft.client.render.VertexConsumer;
+
+public class ParticleVertexWriterFallback extends VertexWriterFallback implements ParticleVertexSink {
+    public ParticleVertexWriterFallback(VertexConsumer consumer) {
+        super(consumer);
+    }
+
+    @Override
+    public void writeParticle(float x, float y, float z, float u, float v, int color, int light) {
+        VertexConsumer consumer = this.consumer;
+        consumer.vertex(x, y, z);
+        consumer.texture(u, v);
+        consumer.color(ColorABGR.unpackRed(color), ColorABGR.unpackGreen(color), ColorABGR.unpackBlue(color), ColorABGR.unpackAlpha(color));
+        consumer.light(light);
+        consumer.next();
+    }
+}

src/main/java/me/jellysquid/mods/sodium/mixin/features/buffer_builder/intrinsics/MixinSpriteTexturedVertexConsumer.java
@@ -1,7 +1,9 @@
 package me.jellysquid.mods.sodium.mixin.features.buffer_builder.intrinsics;
 
-import me.jellysquid.mods.sodium.client.model.consumer.ParticleVertexConsumer;
-import me.jellysquid.mods.sodium.client.model.consumer.QuadVertexConsumer;
+import me.jellysquid.mods.sodium.client.model.vertex.VertexDrain;
+import me.jellysquid.mods.sodium.client.model.vertex.VertexSink;
+import me.jellysquid.mods.sodium.client.model.vertex.VertexSinkFactory;
+import me.jellysquid.mods.sodium.client.model.vertex.transformers.SpriteTexturedVertexTransformer;
 import net.minecraft.client.render.SpriteTexturedVertexConsumer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.texture.Sprite;
@@ -10,29 +12,20 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(SpriteTexturedVertexConsumer.class)
-public abstract class MixinSpriteTexturedVertexConsumer implements QuadVertexConsumer, ParticleVertexConsumer {
-    @Shadow
-    @Final
-    private VertexConsumer parent;
-
+public abstract class MixinSpriteTexturedVertexConsumer implements VertexDrain {
     @Shadow
     @Final
     private Sprite sprite;
 
-    @Override
-    public void vertexQuad(float x, float y, float z, int color, float u, float v, int light, int overlay, int norm) {
-        u = this.sprite.getFrameU(u * 16.0F);
-        v = this.sprite.getFrameV(v * 16.0F);
-
-        ((QuadVertexConsumer) this.parent).vertexQuad(x, y, z, color, u, v, light, overlay, norm);
-    }
+    @Shadow
+    @Final
+    private VertexConsumer parent;
 
     @Override
-    public void vertexParticle(float x, float y, float z, float u, float v, int color, int light) {
-        u = this.sprite.getFrameU(u * 16.0F);
-        v = this.sprite.getFrameV(v * 16.0F);
+    public <T extends VertexSink> T createSink(VertexSinkFactory<T> factory) {
+        T sink = VertexDrain.of(this.parent)
+                .createSink(factory);
 
-        ((ParticleVertexConsumer) this.parent).vertexParticle(x, y, z, u, v, color, light);
+        return factory.createTransformingSink(sink, new SpriteTexturedVertexTransformer(this.sprite));
     }
-
 }

src/main/java/me/jellysquid/mods/sodium/mixin/features/entity/fast_render/MixinBufferRenderer.java
@@ -1,0 +1,46 @@
+package me.jellysquid.mods.sodium.mixin.features.entity.fast_render;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import me.jellysquid.mods.sodium.client.gl.buffer.GlMutableBuffer;
+import net.minecraft.client.render.BufferRenderer;
+import net.minecraft.client.render.VertexFormat;
+import org.lwjgl.opengl.GL15;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.nio.ByteBuffer;
+
+@Mixin(BufferRenderer.class)
+public class MixinBufferRenderer {
+    private static GlMutableBuffer globalGpuBuffer;
+
+    /**
+     * @author JellySquid
+     * @reason Avoid client side memory to speed up rendering
+     */
+    @Overwrite
+    private static void draw(ByteBuffer buffer, int mode, VertexFormat vertexFormat, int count) {
+        buffer.clear();
+
+        if (count <= 0) {
+            return;
+        }
+
+        if (globalGpuBuffer == null) {
+            globalGpuBuffer = new GlMutableBuffer(GL15.GL_STREAM_DRAW);
+        }
+
+        GlMutableBuffer glBuffer = globalGpuBuffer;
+        glBuffer.bind(GL15.GL_ARRAY_BUFFER);
+        glBuffer.upload(GL15.GL_ARRAY_BUFFER, buffer);
+
+        vertexFormat.startDrawing(0L);
+
+        GlStateManager.drawArrays(mode, 0, count);
+
+        vertexFormat.endDrawing();
+
+        glBuffer.invalidate(GL15.GL_ARRAY_BUFFER);
+        glBuffer.unbind(GL15.GL_ARRAY_BUFFER);
+    }
+}

spoorn/sodium-forge

src/main/java/me/jellysquid/mods/sodium/client/model/vertex/formats/glyph/writer/GlyphVertexWriterFallback.java
@@ -1,0 +1,22 @@
+package me.jellysquid.mods.sodium.client.model.vertex.formats.glyph.writer;
+
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import me.jellysquid.mods.sodium.client.model.vertex.fallback.VertexWriterFallback;
+import me.jellysquid.mods.sodium.client.model.vertex.formats.glyph.GlyphVertexSink;
+import me.jellysquid.mods.sodium.client.util.color.ColorABGR;
+
+public class GlyphVertexWriterFallback extends VertexWriterFallback implements GlyphVertexSink {
+    public GlyphVertexWriterFallback(IVertexBuilder consumer) {
+        super(consumer);
+    }
+
+    @Override
+    public void writeGlyph(float x, float y, float z, int color, float u, float v, int light) {
+        IVertexBuilder consumer = this.consumer;
+        consumer.pos(x, y, z);
+        consumer.color(ColorABGR.unpackRed(color), ColorABGR.unpackGreen(color), ColorABGR.unpackBlue(color), ColorABGR.unpackAlpha(color));
+        consumer.tex(u, v);
+        consumer.lightmap(light);
+        consumer.endVertex();
+    }
+}

src/main/java/me/jellysquid/mods/sodium/client/model/vertex/formats/particle/writer/ParticleVertexWriterFallback.java
@@ -1,0 +1,22 @@
+package me.jellysquid.mods.sodium.client.model.vertex.formats.particle.writer;
+
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import me.jellysquid.mods.sodium.client.model.vertex.fallback.VertexWriterFallback;
+import me.jellysquid.mods.sodium.client.model.vertex.formats.particle.ParticleVertexSink;
+import me.jellysquid.mods.sodium.client.util.color.ColorABGR;
+
+public class ParticleVertexWriterFallback extends VertexWriterFallback implements ParticleVertexSink {
+    public ParticleVertexWriterFallback(IVertexBuilder consumer) {
+        super(consumer);
+    }
+
+    @Override
+    public void writeParticle(float x, float y, float z, float u, float v, int color, int light) {
+        IVertexBuilder consumer = this.consumer;
+        consumer.pos(x, y, z);
+        consumer.tex(u, v);
+        consumer.color(ColorABGR.unpackRed(color), ColorABGR.unpackGreen(color), ColorABGR.unpackBlue(color), ColorABGR.unpackAlpha(color));
+        consumer.lightmap(light);
+        consumer.endVertex();
+    }
+}

src/main/java/me/jellysquid/mods/sodium/mixin/features/buffer_builder/intrinsics/MixinSpriteTexturedVertexConsumer.java
@@ -1,8 +1,10 @@
 package me.jellysquid.mods.sodium.mixin.features.buffer_builder.intrinsics;
 
 import com.mojang.blaze3d.vertex.IVertexBuilder;
-import me.jellysquid.mods.sodium.client.model.consumer.ParticleVertexConsumer;
-import me.jellysquid.mods.sodium.client.model.consumer.QuadVertexConsumer;
+import me.jellysquid.mods.sodium.client.model.vertex.VertexDrain;
+import me.jellysquid.mods.sodium.client.model.vertex.VertexSink;
+import me.jellysquid.mods.sodium.client.model.vertex.VertexSinkFactory;
+import me.jellysquid.mods.sodium.client.model.vertex.transformers.SpriteTexturedVertexTransformer;
 import net.minecraft.client.renderer.SpriteAwareVertexBuilder;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import org.spongepowered.asm.mixin.Final;
@@ -10,29 +12,20 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(SpriteAwareVertexBuilder.class)
-public abstract class MixinSpriteTexturedVertexConsumer implements QuadVertexConsumer, ParticleVertexConsumer {
-    @Shadow
-    @Final
-    private IVertexBuilder vertexBuilder;
-
+public abstract class MixinSpriteTexturedVertexConsumer implements VertexDrain {
     @Shadow
     @Final
     private TextureAtlasSprite atlasSprite;
 
-    @Override
-    public void vertexQuad(float x, float y, float z, int color, float u, float v, int light, int overlay, int norm) {
-        u = this.atlasSprite.getInterpolatedU(u * 16.0F);
-        v = this.atlasSprite.getInterpolatedV(v * 16.0F);
-
-        ((QuadVertexConsumer) this.vertexBuilder).vertexQuad(x, y, z, color, u, v, light, overlay, norm);
-    }
+    @Shadow
+    @Final
+    private IVertexBuilder vertexBuilder;
 
     @Override
-    public void vertexParticle(float x, float y, float z, float u, float v, int color, int light) {
-        u = this.atlasSprite.getInterpolatedU(u * 16.0F);
-        v = this.atlasSprite.getInterpolatedV(v * 16.0F);
+    public <T extends VertexSink> T createSink(VertexSinkFactory<T> factory) {
+        T sink = VertexDrain.of(this.vertexBuilder)
+                .createSink(factory);
 
-        ((ParticleVertexConsumer) this.vertexBuilder).vertexParticle(x, y, z, u, v, color, light);
+        return factory.createTransformingSink(sink, new SpriteTexturedVertexTransformer(this.atlasSprite));
     }
-
 }

src/main/java/me/jellysquid/mods/sodium/mixin/features/entity/fast_render/MixinBufferRenderer.java
@@ -1,0 +1,46 @@
+package me.jellysquid.mods.sodium.mixin.features.entity.fast_render;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import me.jellysquid.mods.sodium.client.gl.buffer.GlMutableBuffer;
+import net.minecraft.client.renderer.WorldVertexBufferUploader;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import org.lwjgl.opengl.GL15;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.nio.ByteBuffer;
+
+@Mixin(WorldVertexBufferUploader.class)
+public class MixinBufferRenderer {
+    private static GlMutableBuffer globalGpuBuffer;
+
+    /**
+     * @author JellySquid
+     * @reason Avoid client side memory to speed up rendering
+     */
+    @Overwrite
+    private static void draw(ByteBuffer buffer, int mode, VertexFormat vertexFormat, int count) {
+        buffer.clear();
+
+        if (count <= 0) {
+            return;
+        }
+
+        if (globalGpuBuffer == null) {
+            globalGpuBuffer = new GlMutableBuffer(GL15.GL_STREAM_DRAW);
+        }
+
+        GlMutableBuffer glBuffer = globalGpuBuffer;
+        glBuffer.bind(GL15.GL_ARRAY_BUFFER);
+        glBuffer.upload(GL15.GL_ARRAY_BUFFER, buffer);
+
+        vertexFormat.setupBufferState(0L);
+
+        GlStateManager.drawArrays(mode, 0, count);
+
+        vertexFormat.clearBufferState();
+
+        glBuffer.invalidate(GL15.GL_ARRAY_BUFFER);
+        glBuffer.unbind(GL15.GL_ARRAY_BUFFER);
+    }
+}