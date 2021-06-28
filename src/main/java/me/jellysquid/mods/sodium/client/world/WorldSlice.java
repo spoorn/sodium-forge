CaffeineMC/sodium-fabric

fix: Use block height to check for invalid subchunks and remove unnecessary height check (#426)
* fix: Check for invalid chunk sections using block height (#425)
Currently, invalid chunk sections are checked for using the chunk position. However, the method used expects a block position instead. Because of this, a crash can occur if the client attempts to render in the uppermost chunk (256-272) above world height since the check uses block height (0-255) instead of chunk height (0-15). This means an invalid chunk height of 16 (256-272 block height) will pass the check incorrectly. To fix this, a single line change is made to use the block height for the check instead of the chunk height.

* fix: Remove unnecessary height check
Currently, a check is present to ensure light arrays are only populated in valid subchunks. However, the method used to check for valid subchunks expects a block position instead of a chunk position. This means an invalid chunk height of 16 (256-272 block height) will pass the check (0-255 block height) incorrectly. While this may be fixed by using the block position instead of the chunk position, it will cause block faces between the valid and invalid subchunks to appear completely dark because of unpopulated light arrays in the chunks adjacent to the valid ones. The vanilla behavior allows the chunks directly under and above the valid subchunks to have a populated light array, allowing light to appear there. Since no chunks outside this range are iterated over anyways, we can safely remove the check completely.

src/main/java/me/jellysquid/mods/sodium/client/world/WorldSlice.java
@@ -216,10 +216,6 @@ public class WorldSlice extends ReusableObject implements BlockRenderView, Biome
     }
 
     private void populateLightArrays(int sectionIdx, ChunkSectionPos pos) {
-        if (World.isHeightInvalid(pos.getY())) {
-            return;
-        }
-
         ChunkLightingView blockLightProvider = this.world.getLightingProvider().get(LightType.BLOCK);
         ChunkLightingView skyLightProvider = this.world.getLightingProvider().get(LightType.SKY);
         
@@ -454,7 +450,7 @@ public class WorldSlice extends ReusableObject implements BlockRenderView, Biome
     private static ChunkSection getChunkSection(Chunk chunk, ChunkSectionPos pos) {
         ChunkSection section = null;
 
-        if (!World.isHeightInvalid(pos.getY())) {
+        if (!World.isHeightInvalid(ChunkSectionPos.getBlockCoord(pos.getY()))) {
             section = chunk.getSectionArray()[pos.getY()];
         }
 

spoorn/sodium-forge

src/main/java/me/jellysquid/mods/sodium/client/world/WorldSlice.java
@@ -428,7 +428,7 @@ public class WorldSlice extends ReusableObject implements IBlockDisplayReader, B
     private static ChunkSection getChunkSection(Chunk chunk, SectionPos pos) {
         ChunkSection section = null;
 
-        if (!World.isYOutOfBounds(pos.getY())) {
+        if (!World.isYOutOfBounds(SectionPos.toWorld(pos.getY()))) {
             section = chunk.getSections()[pos.getY()];
         }
 