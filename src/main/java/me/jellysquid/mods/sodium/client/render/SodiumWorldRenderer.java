jan-leila/sodium-fabric

fix: Respect entity render distance setting

src/main/java/me/jellysquid/mods/sodium/client/render/SodiumWorldRenderer.java
@@ -215,7 +215,7 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
 
         profiler.pop();
 
-        Entity.setRenderDistanceMultiplier(MathHelper.clamp((double) this.client.options.viewDistance / 8.0D, 1.0D, 2.5D));
+        Entity.setRenderDistanceMultiplier(MathHelper.clamp((double) this.client.options.viewDistance / 8.0D, 1.0D, 2.5D) * (double) this.client.options.entityDistanceScaling);
     }
 
     /**

spoorn/sodium-forge

src/main/java/me/jellysquid/mods/sodium/client/render/SodiumWorldRenderer.java
@@ -226,7 +226,7 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
 
         profiler.endSection();
 
-        Entity.setRenderDistanceWeight(MathHelper.clamp((double) this.client.gameSettings.renderDistanceChunks / 8.0D, 1.0D, 2.5D));
+        Entity.setRenderDistanceWeight(MathHelper.clamp((double) this.client.gameSettings.renderDistanceChunks / 8.0D, 1.0D, 2.5D) * (double) this.client.gameSettings.entityDistanceScaling);
     }
 
 