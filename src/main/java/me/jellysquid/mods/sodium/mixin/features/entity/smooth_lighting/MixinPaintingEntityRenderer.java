7bb20d5c353ea3e0d2e1186ce26fcedcd040ef9a
spoorn <michael.trinh1996@gmail.com>
2021-06-29 02:09:31 -0700

    Fix cherry pick conflicts

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/entity/smooth_lighting/MixinPaintingEntityRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/entity/smooth_lighting/MixinPaintingEntityRenderer.java
@@ -1,49 +1,49 @@
 package me.jellysquid.mods.sodium.mixin.features.entity.smooth_lighting;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.gui.SodiumGameOptions;
 import me.jellysquid.mods.sodium.client.model.light.EntityLighter;
 import me.jellysquid.mods.sodium.client.render.entity.EntityLightSampler;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
-import net.minecraft.client.render.entity.EntityRenderer;
-import net.minecraft.client.render.entity.PaintingEntityRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.decoration.painting.PaintingEntity;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.PaintingRenderer;
+import net.minecraft.entity.item.PaintingEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockRenderView;
+import net.minecraft.world.IBlockDisplayReader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PaintingEntityRenderer.class)
+@Mixin(PaintingRenderer.class)
 public abstract class MixinPaintingEntityRenderer extends EntityRenderer<PaintingEntity> implements EntityLightSampler<PaintingEntity> {
     private PaintingEntity entity;
     private float tickDelta;
 
-    protected MixinPaintingEntityRenderer(EntityRenderDispatcher dispatcher) {
+    protected MixinPaintingEntityRenderer(EntityRendererManager dispatcher) {
         super(dispatcher);
     }
 
     @Inject(method = "render", at = @At(value = "HEAD"))
-    public void preRender(PaintingEntity paintingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
-        this.entity = paintingEntity;
-        this.tickDelta = g;
+    public void preRender(PaintingEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, CallbackInfo ci) {
+        this.entity = entityIn;
+        this.tickDelta = partialTicks;
     }
 
     /**
      * @author FlashyReese
      * @reason Redirect Lightmap coord with Sodium's EntityLighter.
      */
-    @Redirect(method = "method_4074", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;getLightmapCoordinates(Lnet/minecraft/world/BlockRenderView;Lnet/minecraft/util/math/BlockPos;)I"))
-    public int redirectLightmapCoord(BlockRenderView world, BlockPos pos) {
+    @Redirect(method = "func_229122_a_", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/WorldRenderer;getCombinedLight(Lnet/minecraft/world/IBlockDisplayReader;Lnet/minecraft/util/math/BlockPos;)I"))
+    public int redirectLightmapCoord(IBlockDisplayReader lightReaderIn, BlockPos blockPosIn) {
         if (SodiumClientMod.options().quality.smoothLighting == SodiumGameOptions.LightingQuality.HIGH) {
             return EntityLighter.getBlendedLight(this, this.entity, tickDelta);
         } else {
-            return WorldRenderer.getLightmapCoordinates(world, pos);
+            return WorldRenderer.getCombinedLight(lightReaderIn, blockPosIn);
         }
     }
 
@@ -54,6 +54,6 @@ public abstract class MixinPaintingEntityRenderer extends EntityRenderer<Paintin
 
     @Override
     public int bridge$getSkyLight(PaintingEntity entity, BlockPos pos) {
-        return this.method_27950(entity, pos);
+        return this.getSkyLight(entity, pos);
     }
 }
