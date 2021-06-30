CaffeineMC/sodium-fabric

change: Deprecate legacy OpenGL, update usages to Core classes
Also fixes some issues in ThinGL where extensions weren't being used.

src/main/java/me/jellysquid/mods/sodium/client/gl/compat/LegacyMatrixStackHelper.java
@@ -1,0 +1,47 @@
+package me.jellysquid.mods.sodium.client.gl.compat;
+
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Matrix4f;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL20C;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.FloatBuffer;
+
+/**
+ * Deprecated functions used for extracting current matrix state from OpenGL. These are not supported in OpenGL Core.
+ */
+@Deprecated
+public class LegacyMatrixStackHelper {
+    /**
+     * Obtains a model-view-projection matrix by multiplying the projection matrix in OpenGL state with the model-view
+     * matrix from {@param matrices}.
+     *
+     * The returned buffer is only valid for the lifetime of {@param stack}.
+     *
+     * @return A float-buffer on the stack containing the model-view-projection matrix in a format suitable for
+     * uploading as uniform state
+     */
+    public static FloatBuffer getModelViewProjectionMatrix(MatrixStack.Entry matrices, MemoryStack stack) {
+        FloatBuffer bufProjection = stack.mallocFloat(16);
+
+        // Since vanilla doesn't expose the projection matrix anywhere, we need to grab it from the OpenGL state
+        // This isn't super fast, but should be sufficient enough to remain compatible with any state modifying code
+        GL20C.glGetFloatv(GL20.GL_PROJECTION_MATRIX, bufProjection);
+
+        FloatBuffer bufModelView = stack.mallocFloat(16);
+        FloatBuffer bufModelViewProjection = stack.mallocFloat(16);
+
+        Matrix4f modelMatrix = matrices.getModel();
+        modelMatrix.writeToBuffer(bufModelView);
+
+        // Use OpenGL to multiply our matrices together
+        GL20.glPushMatrix();
+        GL20.glLoadMatrixf(bufProjection);
+        GL20.glMultMatrixf(bufModelView);
+        GL20.glGetFloatv(GL20.GL_MODELVIEW_MATRIX, bufModelViewProjection);
+        GL20.glPopMatrix();
+
+        return bufModelViewProjection;
+    }
+}

src/main/java/me/jellysquid/mods/sodium/mixin/features/gui/MixinDebugHud.java
@@ -9,7 +9,7 @@ import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Matrix4f;
 import org.apache.commons.lang3.Validate;
-import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20C;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -95,7 +95,7 @@ public abstract class MixinDebugHud {
         float k = (float) (color & 255) / 255.0F;
 
         BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
-        bufferBuilder.begin(GL11.GL_QUADS, VertexFormats.POSITION_COLOR);
+        bufferBuilder.begin(GL20C.GL_QUADS, VertexFormats.POSITION_COLOR);
 
         Matrix4f matrix = matrixStack.peek()
                 .getModel();

spoorn/sodium-forge

src/main/java/me/jellysquid/mods/sodium/client/gl/compat/LegacyMatrixStackHelper.java
@@ -1,0 +1,47 @@
+package me.jellysquid.mods.sodium.client.gl.compat;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.util.math.vector.Matrix4f;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL20C;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.FloatBuffer;
+
+/**
+ * Deprecated functions used for extracting current matrix state from OpenGL. These are not supported in OpenGL Core.
+ */
+@Deprecated
+public class LegacyMatrixStackHelper {
+    /**
+     * Obtains a model-view-projection matrix by multiplying the projection matrix in OpenGL state with the model-view
+     * matrix from {@param matrices}.
+     *
+     * The returned buffer is only valid for the lifetime of {@param stack}.
+     *
+     * @return A float-buffer on the stack containing the model-view-projection matrix in a format suitable for
+     * uploading as uniform state
+     */
+    public static FloatBuffer getModelViewProjectionMatrix(MatrixStack.Entry matrices, MemoryStack stack) {
+        FloatBuffer bufProjection = stack.mallocFloat(16);
+
+        // Since vanilla doesn't expose the projection matrix anywhere, we need to grab it from the OpenGL state
+        // This isn't super fast, but should be sufficient enough to remain compatible with any state modifying code
+        GL20C.glGetFloatv(GL20.GL_PROJECTION_MATRIX, bufProjection);
+
+        FloatBuffer bufModelView = stack.mallocFloat(16);
+        FloatBuffer bufModelViewProjection = stack.mallocFloat(16);
+
+        Matrix4f modelMatrix = matrices.getMatrix();
+        modelMatrix.write(bufModelView);
+
+        // Use OpenGL to multiply our matrices together
+        GL20.glPushMatrix();
+        GL20.glLoadMatrixf(bufProjection);
+        GL20.glMultMatrixf(bufModelView);
+        GL20.glGetFloatv(GL20.GL_MODELVIEW_MATRIX, bufModelViewProjection);
+        GL20.glPopMatrix();
+
+        return bufModelViewProjection;
+    }
+}

src/main/java/me/jellysquid/mods/sodium/mixin/features/gui/MixinDebugHud.java
@@ -13,7 +13,7 @@ import net.minecraft.client.renderer.WorldVertexBufferUploader;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.math.vector.Matrix4f;
 import org.apache.commons.lang3.Validate;
-import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20C;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -99,7 +99,7 @@ public abstract class MixinDebugHud {
         float k = (float) (color & 255) / 255.0F;
 
         BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
-        bufferBuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_COLOR);
+        bufferBuilder.begin(GL20C.GL_QUADS, DefaultVertexFormats.POSITION_COLOR);
 
         Matrix4f matrix = matrixStack.getLast()
                 .getMatrix();