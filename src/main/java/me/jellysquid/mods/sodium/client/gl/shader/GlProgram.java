CaffeineMC/sodium-fabric

new: Initial "thin GL" implementation

src/main/java/me/jellysquid/mods/sodium/client/gl/shader/GlProgram.java
@@ -2,6 +2,8 @@ package me.jellysquid.mods.sodium.client.gl.shader;
 
 import me.jellysquid.mods.sodium.client.gl.GlObject;
 import me.jellysquid.mods.sodium.client.gl.attribute.GlVertexAttribute;
+import me.jellysquid.mods.sodium.client.gl.device.RenderDevice;
+import me.jellysquid.mods.sodium.client.render.chunk.shader.ChunkShaderBindingPoints;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,7 +18,9 @@ public abstract class GlProgram extends GlObject {
 
     private final Identifier name;
 
-    protected GlProgram(Identifier name, int program) {
+    protected GlProgram(RenderDevice owner, Identifier name, int program) {
+        super(owner);
+
         this.name = name;
         this.setHandle(program);
     }
@@ -48,22 +52,6 @@ public abstract class GlProgram extends GlObject {
 
         if (index < 0) {
             throw new NullPointerException("No uniform exists with name: " + name);
-        }
-
-        return index;
-    }
-
-    /**
-     * Retrieves the index of the vertex attribute with the given name.
-     * @param name The name of the attribute to find the index of
-     * @return The attribute's index
-     * @throws NullPointerException If no attribute exists with the given name
-     */
-    protected int getAttributeLocation(String name) {
-        int index = GL20.glGetAttribLocation(this.handle(), name);
-
-        if (index < 0) {
-            throw new NullPointerException("No attribute exists with name: " + name);
         }
 
         return index;
@@ -117,8 +105,8 @@ public abstract class GlProgram extends GlObject {
             return factory.create(this.name, this.program);
         }
 
-        public Builder bindAttribute(String name, GlVertexAttribute attribute) {
-            GL20.glBindAttribLocation(this.program, attribute.getIndex(), name);
+        public Builder bindAttribute(String name, ShaderBindingPoint binding) {
+            GL20.glBindAttribLocation(this.program, binding.getGenericAttributeIndex(), name);
 
             return this;
         }

src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkProgram.java
@@ -1,7 +1,7 @@
 package me.jellysquid.mods.sodium.client.render.chunk.shader;
 
-import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.gl.shader.GlProgram;
+import me.jellysquid.mods.sodium.client.gl.device.RenderDevice;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import org.lwjgl.opengl.GL11;
@@ -15,7 +15,7 @@ import java.util.function.Function;
 /**
  * A forward-rendering shader program for chunks.
  */
-public abstract class ChunkProgram extends GlProgram {
+public class ChunkProgram extends GlProgram {
     // Uniform variable binding indexes
     private final int uModelViewProjectionMatrix;
     private final int uModelScale;
@@ -26,8 +26,8 @@ public abstract class ChunkProgram extends GlProgram {
     // The fog shader component used by this program in order to setup the appropriate GL state
     private final ChunkShaderFogComponent fogShader;
 
-    protected ChunkProgram(Identifier name, int handle, Function<ChunkProgram, ChunkShaderFogComponent> fogShaderFunction) {
-        super(name, handle);
+    protected ChunkProgram(RenderDevice owner, Identifier name, int handle, Function<ChunkProgram, ChunkShaderFogComponent> fogShaderFunction) {
+        super(owner, name, handle);
 
         this.uModelViewProjectionMatrix = this.getUniformLocation("u_ModelViewProjectionMatrix");
 

src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkRenderShaderBackend.java
@@ -3,6 +3,9 @@ package me.jellysquid.mods.sodium.client.render.chunk.shader;
 import me.jellysquid.mods.sodium.client.gl.attribute.GlVertexFormat;
 import me.jellysquid.mods.sodium.client.gl.shader.GlProgram;
 import me.jellysquid.mods.sodium.client.gl.shader.GlShader;
+import me.jellysquid.mods.sodium.client.gl.device.RenderDevice;
+import me.jellysquid.mods.sodium.client.gl.shader.ShaderLoader;
+import me.jellysquid.mods.sodium.client.gl.shader.ShaderType;
 import me.jellysquid.mods.sodium.client.model.vertex.type.ChunkVertexType;
 import me.jellysquid.mods.sodium.client.render.chunk.ChunkGraphicsState;
 import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderBackend;
@@ -12,51 +15,49 @@ import net.minecraft.util.Identifier;
 
 import java.util.EnumMap;
 
-public abstract class ChunkRenderShaderBackend<T extends ChunkGraphicsState, P extends ChunkProgram>
+public abstract class ChunkRenderShaderBackend<T extends ChunkGraphicsState>
         implements ChunkRenderBackend<T> {
-    private final EnumMap<ChunkFogMode, P> programs = new EnumMap<>(ChunkFogMode.class);
+    private final EnumMap<ChunkFogMode, ChunkProgram> programs = new EnumMap<>(ChunkFogMode.class);
 
     protected final ChunkVertexType vertexType;
     protected final GlVertexFormat<ChunkMeshAttribute> vertexFormat;
 
-    protected P activeProgram;
+    protected ChunkProgram activeProgram;
 
     public ChunkRenderShaderBackend(ChunkVertexType vertexType) {
         this.vertexType = vertexType;
         this.vertexFormat = vertexType.getCustomVertexFormat();
     }
 
-    @Override
-    public final void createShaders() {
-        this.programs.put(ChunkFogMode.NONE, this.createShader(ChunkFogMode.NONE, this.vertexFormat));
-        this.programs.put(ChunkFogMode.LINEAR, this.createShader(ChunkFogMode.LINEAR, this.vertexFormat));
-        this.programs.put(ChunkFogMode.EXP2, this.createShader(ChunkFogMode.EXP2, this.vertexFormat));
-    }
+    private ChunkProgram createShader(RenderDevice device, ChunkFogMode fogMode, GlVertexFormat<ChunkMeshAttribute> vertexFormat) {
+        GlShader vertShader = ShaderLoader.loadShader(device, ShaderType.VERTEX,
+                new Identifier("sodium", "chunk_gl20.v.glsl"), fogMode.getDefines());
 
-    private P createShader(ChunkFogMode fogMode, GlVertexFormat<ChunkMeshAttribute> format) {
-        GlShader vertShader = this.createVertexShader(fogMode);
-        GlShader fragShader = this.createFragmentShader(fogMode);
+        GlShader fragShader = ShaderLoader.loadShader(device, ShaderType.FRAGMENT,
+                new Identifier("sodium", "chunk_gl20.f.glsl"), fogMode.getDefines());
 
         try {
             return GlProgram.builder(new Identifier("sodium", "chunk_shader"))
                     .attachShader(vertShader)
                     .attachShader(fragShader)
-                    .bindAttribute("a_Pos", format.getAttribute(ChunkMeshAttribute.POSITION))
-                    .bindAttribute("a_Color", format.getAttribute(ChunkMeshAttribute.COLOR))
-                    .bindAttribute("a_TexCoord", format.getAttribute(ChunkMeshAttribute.TEXTURE))
-                    .bindAttribute("a_LightCoord", format.getAttribute(ChunkMeshAttribute.LIGHT))
-                    .build((program, name) -> this.createShaderProgram(program, name, fogMode));
+                    .bindAttribute("a_Pos", ChunkShaderBindingPoints.POSITION)
+                    .bindAttribute("a_Color", ChunkShaderBindingPoints.COLOR)
+                    .bindAttribute("a_TexCoord", ChunkShaderBindingPoints.TEX_COORD)
+                    .bindAttribute("a_LightCoord", ChunkShaderBindingPoints.LIGHT_COORD)
+                    .bindAttribute("d_ModelOffset", ChunkShaderBindingPoints.MODEL_OFFSET)
+                    .build((program, name) -> new ChunkProgram(device, program, name, fogMode.getFactory()));
         } finally {
             vertShader.delete();
             fragShader.delete();
         }
     }
 
-    protected abstract GlShader createFragmentShader(ChunkFogMode fogMode);
-
-    protected abstract GlShader createVertexShader(ChunkFogMode fogMode);
-
-    protected abstract P createShaderProgram(Identifier name, int handle, ChunkFogMode fogMode);
+    @Override
+    public final void createShaders(RenderDevice device) {
+        this.programs.put(ChunkFogMode.NONE, this.createShader(device, ChunkFogMode.NONE, this.vertexFormat));
+        this.programs.put(ChunkFogMode.LINEAR, this.createShader(device, ChunkFogMode.LINEAR, this.vertexFormat));
+        this.programs.put(ChunkFogMode.EXP2, this.createShader(device, ChunkFogMode.EXP2, this.vertexFormat));
+    }
 
     @Override
     public void begin(MatrixStack matrixStack) {
@@ -73,7 +74,7 @@ public abstract class ChunkRenderShaderBackend<T extends ChunkGraphicsState, P e
 
     @Override
     public void delete() {
-        for (P shader : this.programs.values()) {
+        for (ChunkProgram shader : this.programs.values()) {
             shader.delete();
         }
     }

spoorn/sodium-forge

src/main/java/me/jellysquid/mods/sodium/client/gl/shader/GlProgram.java
@@ -3,7 +3,7 @@ package me.jellysquid.mods.sodium.client.gl.shader;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import me.jellysquid.mods.sodium.client.gl.GlObject;
-import me.jellysquid.mods.sodium.client.gl.attribute.GlVertexAttribute;
+import me.jellysquid.mods.sodium.client.gl.device.RenderDevice;
 import net.minecraft.util.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,7 +20,9 @@ public abstract class GlProgram extends GlObject {
 
     private final ResourceLocation name;
 
-    protected GlProgram(ResourceLocation name, int program) {
+    protected GlProgram(RenderDevice owner, ResourceLocation name, int program) {
+        super(owner);
+
         this.name = name;
         this.setHandle(program);
     }
@@ -52,22 +54,6 @@ public abstract class GlProgram extends GlObject {
 
         if (index < 0) {
             throw new NullPointerException("No uniform exists with name: " + name);
-        }
-
-        return index;
-    }
-
-    /**
-     * Retrieves the index of the vertex attribute with the given name.
-     * @param name The name of the attribute to find the index of
-     * @return The attribute's index
-     * @throws NullPointerException If no attribute exists with the given name
-     */
-    protected int getAttributeLocation(String name) {
-        int index = GL20.glGetAttribLocation(this.handle(), name);
-
-        if (index < 0) {
-            throw new NullPointerException("No attribute exists with name: " + name);
         }
 
         return index;
@@ -127,8 +113,8 @@ public abstract class GlProgram extends GlObject {
             return factory.create(this.name, this.program);
         }
 
-        public Builder bindAttribute(String name, GlVertexAttribute attribute) {
-            GL20.glBindAttribLocation(this.program, attribute.getIndex(), name);
+        public Builder bindAttribute(String name, ShaderBindingPoint binding) {
+            GL20.glBindAttribLocation(this.program, binding.getGenericAttributeIndex(), name);
 
             return this;
         }

src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkProgram.java
@@ -1,6 +1,7 @@
 package me.jellysquid.mods.sodium.client.render.chunk.shader;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import me.jellysquid.mods.sodium.client.gl.device.RenderDevice;
 import me.jellysquid.mods.sodium.client.gl.shader.GlProgram;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Matrix4f;
@@ -13,7 +14,7 @@ import java.util.function.Function;
 /**
  * A forward-rendering shader program for chunks.
  */
-public abstract class ChunkProgram extends GlProgram {
+public class ChunkProgram extends GlProgram {
     // Uniform variable binding indexes
     private final int uModelViewProjectionMatrix;
     private final int uModelScale;
@@ -24,8 +25,8 @@ public abstract class ChunkProgram extends GlProgram {
     // The fog shader component used by this program in order to setup the appropriate GL state
     private final ChunkShaderFogComponent fogShader;
 
-    protected ChunkProgram(ResourceLocation name, int handle, Function<ChunkProgram, ChunkShaderFogComponent> fogShaderFunction) {
-        super(name, handle);
+    protected ChunkProgram(RenderDevice owner, ResourceLocation name, int handle, Function<ChunkProgram, ChunkShaderFogComponent> fogShaderFunction) {
+        super(owner, name, handle);
 
         this.uModelViewProjectionMatrix = this.getUniformLocation("u_ModelViewProjectionMatrix");
         this.uBlockTex = this.getUniformLocation("u_BlockTex");

src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkRenderShaderBackend.java
@@ -2,6 +2,7 @@ package me.jellysquid.mods.sodium.client.render.chunk.shader;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import me.jellysquid.mods.sodium.client.gl.attribute.GlVertexFormat;
+import me.jellysquid.mods.sodium.client.gl.device.RenderDevice;
 import me.jellysquid.mods.sodium.client.gl.shader.GlProgram;
 import me.jellysquid.mods.sodium.client.gl.shader.GlShader;
 import me.jellysquid.mods.sodium.client.gl.shader.ShaderLoader;
@@ -15,57 +16,49 @@ import net.minecraft.util.math.vector.Matrix4f;
 
 import java.util.EnumMap;
 
-public abstract class ChunkRenderShaderBackend<T extends ChunkGraphicsState, P extends ChunkProgram>
+public abstract class ChunkRenderShaderBackend<T extends ChunkGraphicsState>
         implements ChunkRenderBackend<T> {
-    private final EnumMap<ChunkFogMode, P> programs = new EnumMap<>(ChunkFogMode.class);
+    private final EnumMap<ChunkFogMode, ChunkProgram> programs = new EnumMap<>(ChunkFogMode.class);
 
     protected final ChunkVertexType vertexType;
     protected final GlVertexFormat<ChunkMeshAttribute> vertexFormat;
 
-    protected P activeProgram;
+    protected ChunkProgram activeProgram;
 
     public ChunkRenderShaderBackend(ChunkVertexType vertexType) {
         this.vertexType = vertexType;
         this.vertexFormat = vertexType.getCustomVertexFormat();
     }
 
-    @Override
-    public final void createShaders() {
-        this.programs.put(ChunkFogMode.NONE, this.createShader(ChunkFogMode.NONE, this.vertexFormat));
-        this.programs.put(ChunkFogMode.LINEAR, this.createShader(ChunkFogMode.LINEAR, this.vertexFormat));
-        this.programs.put(ChunkFogMode.EXP2, this.createShader(ChunkFogMode.EXP2, this.vertexFormat));
-    }
+    private ChunkProgram createShader(RenderDevice device, ChunkFogMode fogMode, GlVertexFormat<ChunkMeshAttribute> vertexFormat) {
+        GlShader vertShader = ShaderLoader.loadShader(device, ShaderType.VERTEX,
+                new ResourceLocation("sodium", "chunk_gl20.v.glsl"), fogMode.getDefines());
 
-    private P createShader(ChunkFogMode fogMode, GlVertexFormat<ChunkMeshAttribute> format) {
-        GlShader vertShader = this.createVertexShader(fogMode);
-        GlShader fragShader = this.createFragmentShader(fogMode);
+        GlShader fragShader = ShaderLoader.loadShader(device, ShaderType.FRAGMENT,
+                new ResourceLocation("sodium", "chunk_gl20.f.glsl"), fogMode.getDefines());
 
         try {
             return GlProgram.builder(new ResourceLocation("sodium", "chunk_shader"))
                     .attachShader(vertShader)
                     .attachShader(fragShader)
-                    .bindAttribute("a_Pos", format.getAttribute(ChunkMeshAttribute.POSITION))
-                    .bindAttribute("a_Color", format.getAttribute(ChunkMeshAttribute.COLOR))
-                    .bindAttribute("a_TexCoord", format.getAttribute(ChunkMeshAttribute.TEXTURE))
-                    .bindAttribute("a_LightCoord", format.getAttribute(ChunkMeshAttribute.LIGHT))
-                    .build((program, name) -> this.createShaderProgram(program, name, fogMode));
+                    .bindAttribute("a_Pos", ChunkShaderBindingPoints.POSITION)
+                    .bindAttribute("a_Color", ChunkShaderBindingPoints.COLOR)
+                    .bindAttribute("a_TexCoord", ChunkShaderBindingPoints.TEX_COORD)
+                    .bindAttribute("a_LightCoord", ChunkShaderBindingPoints.LIGHT_COORD)
+                    .bindAttribute("d_ModelOffset", ChunkShaderBindingPoints.MODEL_OFFSET)
+                    .build((program, name) -> new ChunkProgram(device, program, name, fogMode.getFactory()));
         } finally {
             vertShader.delete();
             fragShader.delete();
         }
     }
 
-    private GlShader createVertexShader(ChunkFogMode fogMode) {
-        return ShaderLoader.loadShader(ShaderType.VERTEX, new ResourceLocation("sodium", "chunk_gl20.v.glsl"),
-               fogMode.getDefines());
+    @Override
+    public final void createShaders(RenderDevice device) {
+        this.programs.put(ChunkFogMode.NONE, this.createShader(device, ChunkFogMode.NONE, this.vertexFormat));
+        this.programs.put(ChunkFogMode.LINEAR, this.createShader(device, ChunkFogMode.LINEAR, this.vertexFormat));
+        this.programs.put(ChunkFogMode.EXP2, this.createShader(device, ChunkFogMode.EXP2, this.vertexFormat));
     }
-
-    private GlShader createFragmentShader(ChunkFogMode fogMode) {
-        return ShaderLoader.loadShader(ShaderType.FRAGMENT, new ResourceLocation("sodium", "chunk_gl20.f.glsl"),
-                fogMode.getDefines());
-    }
-
-    protected abstract P createShaderProgram(ResourceLocation name, int handle, ChunkFogMode fogMode);
 
     @Override
     public void begin(MatrixStack matrixStack, Matrix4f projection) {
@@ -81,7 +74,7 @@ public abstract class ChunkRenderShaderBackend<T extends ChunkGraphicsState, P e
 
     @Override
     public void delete() {
-        for (P shader : this.programs.values()) {
+        for (ChunkProgram shader : this.programs.values()) {
             shader.delete();
         }
     }