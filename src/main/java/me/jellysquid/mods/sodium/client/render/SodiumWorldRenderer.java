CaffeineMC/sodium-fabric

fix: Send chunk unload events when load distance changes
Closes #673

src/main/java/me/jellysquid/mods/sodium/client/render/SodiumWorldRenderer.java
@@ -46,7 +46,6 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
     private final MinecraftClient client;
 
     private ClientWorld world;
-    private int renderDistance;
 
     private double lastCameraX, lastCameraY, lastCameraZ;
     private double lastCameraPitch, lastCameraYaw;
@@ -166,10 +165,6 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
 
         this.useEntityCulling = SodiumClientMod.options().advanced.useAdvancedEntityCulling;
 
-        if (this.client.options.viewDistance != this.renderDistance) {
-            this.reload();
-        }
-
         Profiler profiler = this.client.getProfiler();
         profiler.push("camera_setup");
 
@@ -250,8 +245,6 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
 
         RenderDevice device = RenderDevice.INSTANCE;
 
-        this.renderDistance = this.client.options.viewDistance;
-
         SodiumGameOptions opts = SodiumClientMod.options();
 
         this.renderPassManager = BlockRenderPassManager.createDefaultMappings();
@@ -267,7 +260,7 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
         this.chunkRenderBackend = createChunkRenderBackend(device, opts, vertexFormat);
         this.chunkRenderBackend.createShaders(device);
 
-        this.chunkRenderManager = new ChunkRenderManager<>(this, this.chunkRenderBackend, this.renderPassManager, this.world, this.renderDistance);
+        this.chunkRenderManager = new ChunkRenderManager<>(this, this.chunkRenderBackend, this.renderPassManager, this.world, this.client.options.viewDistance);
         this.chunkRenderManager.restoreChunks(this.loadedChunkPositions);
     }
 

src/main/java/me/jellysquid/mods/sodium/mixin/features/chunk_rendering/MixinClientChunkManager.java
@@ -1,13 +1,18 @@
 package me.jellysquid.mods.sodium.mixin.features.chunk_rendering;
 
+import it.unimi.dsi.fastutil.longs.LongIterator;
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import me.jellysquid.mods.sodium.client.world.ChunkStatusListener;
 import me.jellysquid.mods.sodium.client.world.ChunkStatusListenerManager;
 import net.minecraft.client.world.ClientChunkManager;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.biome.source.BiomeArray;
+import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.chunk.WorldChunk;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,13 +22,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
 @Mixin(ClientChunkManager.class)
-public class MixinClientChunkManager implements ChunkStatusListenerManager {
+public abstract class MixinClientChunkManager implements ChunkStatusListenerManager {
+    @Shadow
+    @Nullable
+    public abstract WorldChunk getChunk(int i, int j, ChunkStatus chunkStatus, boolean bl);
+
+    private final LongOpenHashSet loadedChunks = new LongOpenHashSet();
     private ChunkStatusListener listener;
 
     @Inject(method = "loadChunkFromPacket", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;resetChunkColor(II)V", shift = At.Shift.AFTER))
     private void afterLoadChunkFromPacket(int x, int z, BiomeArray biomes, PacketByteBuf buf, CompoundTag tag, int verticalStripBitmask, boolean complete, CallbackInfoReturnable<WorldChunk> cir) {
         if (this.listener != null) {
             this.listener.onChunkAdded(x, z);
+            this.loadedChunks.add(ChunkPos.toLong(x, z));
         }
     }
 
@@ -31,6 +42,27 @@ public class MixinClientChunkManager implements ChunkStatusListenerManager {
     private void afterUnloadChunk(int x, int z, CallbackInfo ci) {
         if (this.listener != null) {
             this.listener.onChunkRemoved(x, z);
+            this.loadedChunks.remove(ChunkPos.toLong(x, z));
+        }
+    }
+
+    @Inject(method = "updateLoadDistance", at = @At("RETURN"))
+    private void afterLoadDistanceChanged(int loadDistance, CallbackInfo ci) {
+        LongIterator it = this.loadedChunks.iterator();
+
+        while (it.hasNext()) {
+            long pos = it.nextLong();
+
+            int x = ChunkPos.getPackedX(pos);
+            int z = ChunkPos.getPackedZ(pos);
+
+            if (this.getChunk(x, z, ChunkStatus.FULL, false) == null) {
+                it.remove();
+
+                if (this.listener != null) {
+                    this.listener.onChunkRemoved(x, z);
+                }
+            }
         }
     }
 

spoorn/sodium-forge

src/main/java/me/jellysquid/mods/sodium/client/render/SodiumWorldRenderer.java
@@ -54,7 +54,6 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
     private final Minecraft client;
 
     private ClientWorld world;
-    private int renderDistance;
 
     private double lastCameraX, lastCameraY, lastCameraZ;
     private double lastCameraPitch, lastCameraYaw;
@@ -180,10 +179,6 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
         this.chunkRenderManager.setProjection(projection);
         this.useEntityCulling = SodiumClientMod.options().advanced.useAdvancedEntityCulling;
 
-        if (this.client.gameSettings.renderDistanceChunks != this.renderDistance) {
-            this.reload();
-        }
-
         IProfiler profiler = this.client.getProfiler();
         profiler.startSection("camera_setup");
 
@@ -280,8 +275,6 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
 
         RenderDevice device = RenderDevice.INSTANCE;
 
-        this.renderDistance = this.client.gameSettings.renderDistanceChunks;
-
         SodiumGameOptions opts = SodiumClientMod.options();
 
         this.renderPassManager = BlockRenderPassManager.createDefaultMappings();
@@ -297,7 +290,7 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
         this.chunkRenderBackend = createChunkRenderBackend(device, opts, vertexFormat);
         this.chunkRenderBackend.createShaders(device);
 
-        this.chunkRenderManager = new ChunkRenderManager<>(this, this.chunkRenderBackend, this.renderPassManager, this.world, this.renderDistance);
+        this.chunkRenderManager = new ChunkRenderManager<>(this, this.chunkRenderBackend, this.renderPassManager, this.world, this.client.gameSettings.renderDistanceChunks);
         this.chunkRenderManager.restoreChunks(this.loadedChunkPositions);
     }
 

src/main/java/me/jellysquid/mods/sodium/mixin/features/chunk_rendering/MixinClientChunkManager.java
@@ -1,29 +1,40 @@
 package me.jellysquid.mods.sodium.mixin.features.chunk_rendering;
 
+import it.unimi.dsi.fastutil.longs.LongIterator;
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import me.jellysquid.mods.sodium.client.world.ChunkStatusListener;
 import me.jellysquid.mods.sodium.client.world.ChunkStatusListenerManager;
 import net.minecraft.client.multiplayer.ClientChunkProvider;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.biome.BiomeContainer;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkStatus;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
 @Mixin(ClientChunkProvider.class)
-public class MixinClientChunkManager implements ChunkStatusListenerManager {
+public abstract class MixinClientChunkManager implements ChunkStatusListenerManager {
+    @Shadow
+    @Nullable
+    public abstract Chunk getChunk(int i, int j, ChunkStatus chunkStatus, boolean bl);
+
+    private final LongOpenHashSet loadedChunks = new LongOpenHashSet();
     private ChunkStatusListener listener;
 
     @Inject(method = "loadChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;onChunkLoaded(II)V", shift = At.Shift.AFTER))
     private void afterLoadChunkFromPacket(int chunkX, int chunkZ, BiomeContainer biomeContainerIn, PacketBuffer packetIn, CompoundNBT nbtTagIn, int sizeIn, boolean fullChunk, CallbackInfoReturnable<Chunk> cir) {
         if (this.listener != null) {
             this.listener.onChunkAdded(chunkX, chunkZ);
+            this.loadedChunks.add(ChunkPos.asLong(chunkX, chunkZ));
         }
     }
 
@@ -31,6 +42,27 @@ public class MixinClientChunkManager implements ChunkStatusListenerManager {
     private void afterUnloadChunk(int x, int z, CallbackInfo ci) {
         if (this.listener != null) {
             this.listener.onChunkRemoved(x, z);
+            this.loadedChunks.remove(ChunkPos.asLong(x, z));
+        }
+    }
+
+    @Inject(method = "setViewDistance", at = @At("RETURN"))
+    private void afterLoadDistanceChanged(int loadDistance, CallbackInfo ci) {
+        LongIterator it = this.loadedChunks.iterator();
+
+        while (it.hasNext()) {
+            long pos = it.nextLong();
+
+            int x = ChunkPos.getX(pos);
+            int z = ChunkPos.getZ(pos);
+
+            if (this.getChunk(x, z, ChunkStatus.FULL, false) == null) {
+                it.remove();
+
+                if (this.listener != null) {
+                    this.listener.onChunkRemoved(x, z);
+                }
+            }
         }
     }
 