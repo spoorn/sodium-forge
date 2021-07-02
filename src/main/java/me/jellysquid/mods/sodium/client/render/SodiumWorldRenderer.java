CaffeineMC/sodium-fabric

change: Move chunk copying into meshing threads
This is one of my messier commits, but it's late and trying
to break this up is too much work.

- Fixed some occlusion culling problems caused by the chunk building
  step not always updating a position vector, resulting in the occlusion
  data having data set for the previous block position when an air block
  is encountered.

- Split world slicing into two stages to enable faster render task
  creation. This introduces a lot of changes, but primarily means the
  main render thread can simply focus on copying memory to a new thread
  without needing to unpack it immediately.

- Fixed a problem where vertex lighting was reading from the client world object
  directly instead of our copied slice, resulting in horrible performance
  and thread safety problems.

src/main/java/me/jellysquid/mods/sodium/client/render/SodiumWorldRenderer.java
@@ -17,7 +17,7 @@ import me.jellysquid.mods.sodium.client.render.chunk.data.ChunkRenderData;
 import me.jellysquid.mods.sodium.client.render.chunk.format.DefaultModelVertexFormats;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPass;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPassManager;
-import me.jellysquid.mods.sodium.client.render.pipeline.context.GlobalRenderContext;
+import me.jellysquid.mods.sodium.client.render.pipeline.context.ChunkRenderCacheShared;
 import me.jellysquid.mods.sodium.client.util.math.FrustumExtended;
 import me.jellysquid.mods.sodium.client.world.ChunkStatusListener;
 import me.jellysquid.mods.sodium.client.world.ChunkStatusListenerManager;
@@ -108,7 +108,7 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
     private void loadWorld(ClientWorld world) {
         this.world = world;
 
-        GlobalRenderContext.createRenderContext(this.world);
+        ChunkRenderCacheShared.createRenderContext(this.world);
 
         this.initRenderer();
 
@@ -116,7 +116,7 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
     }
 
     private void unloadWorld() {
-        GlobalRenderContext.destroyRenderContext(this.world);
+        ChunkRenderCacheShared.destroyRenderContext(this.world);
 
         if (this.chunkRenderManager != null) {
             this.chunkRenderManager.destroy();
@@ -179,23 +179,20 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
             throw new IllegalStateException("Client instance has no active player entity");
         }
 
-        Vec3d cameraPos = camera.getPos();
-
-        this.chunkRenderManager.setCameraPosition(cameraPos.x, cameraPos.y, cameraPos.z);
-
+        Vec3d pos = camera.getPos();
         float pitch = camera.getPitch();
         float yaw = camera.getYaw();
 
-        boolean dirty = cameraPos.x != this.lastCameraX || cameraPos.y != this.lastCameraY || cameraPos.z != this.lastCameraZ ||
+        boolean dirty = pos.x != this.lastCameraX || pos.y != this.lastCameraY || pos.z != this.lastCameraZ ||
                 pitch != this.lastCameraPitch || yaw != this.lastCameraYaw;
 
         if (dirty) {
             this.chunkRenderManager.markDirty();
         }
 
-        this.lastCameraX = cameraPos.x;
-        this.lastCameraY = cameraPos.y;
-        this.lastCameraZ = cameraPos.z;
+        this.lastCameraX = pos.x;
+        this.lastCameraY = pos.y;
+        this.lastCameraZ = pos.z;
         this.lastCameraPitch = pitch;
         this.lastCameraYaw = yaw;
 

src/main/java/me/jellysquid/mods/sodium/client/render/pipeline/context/ChunkRenderCacheLocal.java
@@ -1,0 +1,57 @@
+package me.jellysquid.mods.sodium.client.render.pipeline.context;
+
+import me.jellysquid.mods.sodium.client.model.light.LightPipelineProvider;
+import me.jellysquid.mods.sodium.client.model.light.cache.ArrayLightDataCache;
+import me.jellysquid.mods.sodium.client.model.quad.blender.BiomeColorBlender;
+import me.jellysquid.mods.sodium.client.render.pipeline.BlockRenderer;
+import me.jellysquid.mods.sodium.client.render.pipeline.ChunkRenderCache;
+import me.jellysquid.mods.sodium.client.render.pipeline.FluidRenderer;
+import me.jellysquid.mods.sodium.client.world.WorldSlice;
+import me.jellysquid.mods.sodium.client.world.cloned.ChunkRenderContext;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.block.BlockModels;
+import net.minecraft.world.World;
+
+public class ChunkRenderCacheLocal extends ChunkRenderCache {
+    private final ArrayLightDataCache lightDataCache;
+
+    private final BlockRenderer blockRenderer;
+    private final FluidRenderer fluidRenderer;
+
+    private final BlockModels blockModels;
+    private final WorldSlice worldSlice;
+
+    public ChunkRenderCacheLocal(MinecraftClient client, World world) {
+        this.worldSlice = new WorldSlice(world);
+        this.lightDataCache = new ArrayLightDataCache(this.worldSlice);
+
+        LightPipelineProvider lightPipelineProvider = new LightPipelineProvider(this.lightDataCache);
+        BiomeColorBlender biomeColorBlender = this.createBiomeColorBlender();
+
+        this.blockRenderer = new BlockRenderer(client, lightPipelineProvider, biomeColorBlender);
+        this.fluidRenderer = new FluidRenderer(client, lightPipelineProvider, biomeColorBlender);
+
+        this.blockModels = client.getBakedModelManager().getBlockModels();
+    }
+
+    public BlockModels getBlockModels() {
+        return this.blockModels;
+    }
+
+    public BlockRenderer getBlockRenderer() {
+        return this.blockRenderer;
+    }
+
+    public FluidRenderer getFluidRenderer() {
+        return this.fluidRenderer;
+    }
+
+    public void init(ChunkRenderContext context) {
+        this.lightDataCache.reset(context.getOrigin());
+        this.worldSlice.copyData(context);
+    }
+
+    public WorldSlice getWorldSlice() {
+        return this.worldSlice;
+    }
+}

src/main/java/me/jellysquid/mods/sodium/client/world/cloned/ChunkRenderContext.java
@@ -1,0 +1,38 @@
+package me.jellysquid.mods.sodium.client.world.cloned;
+
+import net.minecraft.util.math.BlockBox;
+import net.minecraft.util.math.ChunkSectionPos;
+import net.minecraft.world.World;
+
+public class ChunkRenderContext {
+    private final ChunkSectionPos origin;
+    private final ClonedChunkSection[] sections;
+    private final BlockBox volume;
+
+    public ChunkRenderContext(ChunkSectionPos origin, ClonedChunkSection[] sections, BlockBox volume) {
+        this.origin = origin;
+        this.sections = sections;
+        this.volume = volume;
+    }
+
+    public ClonedChunkSection[] getSections() {
+        return this.sections;
+    }
+
+    public ChunkSectionPos getOrigin() {
+        return this.origin;
+    }
+
+    public BlockBox getVolume() {
+        return this.volume;
+    }
+
+    public void releaseResources() {
+        for (ClonedChunkSection section : sections) {
+            if (section != null) {
+                section.getBackingCache()
+                        .release(section);
+            }
+        }
+    }
+}

src/main/java/me/jellysquid/mods/sodium/client/world/cloned/ClonedChunkSectionCache.java
@@ -1,0 +1,71 @@
+package me.jellysquid.mods.sodium.client.world.cloned;
+
+import it.unimi.dsi.fastutil.longs.Long2ReferenceMap;
+import it.unimi.dsi.fastutil.longs.Long2ReferenceOpenHashMap;
+import net.minecraft.util.math.ChunkSectionPos;
+import net.minecraft.world.World;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ClonedChunkSectionCache {
+    private final World world;
+
+    private final ConcurrentLinkedQueue<ClonedChunkSection> inactivePool = new ConcurrentLinkedQueue<>();
+    private final Long2ReferenceMap<ClonedChunkSection> byPosition = new Long2ReferenceOpenHashMap<>();
+
+    public ClonedChunkSectionCache(World world) {
+        this.world = world;
+    }
+
+    public ClonedChunkSection acquire(int x, int y, int z) {
+        long key = ChunkSectionPos.asLong(x, y, z);
+        ClonedChunkSection section = this.byPosition.get(key);
+
+        if (section != null) {
+            this.inactivePool.remove(section);
+        } else {
+            section = this.createSection(x, y, z);
+        }
+
+        section.acquireReference();
+
+        return section;
+    }
+
+    private ClonedChunkSection createSection(int x, int y, int z) {
+        ClonedChunkSection section;
+
+        if (!this.inactivePool.isEmpty()) {
+            section = this.inactivePool.remove();
+
+            this.byPosition.remove(section.getPosition().asLong());
+        } else {
+            section = this.allocate();
+        }
+
+        ChunkSectionPos pos = ChunkSectionPos.from(x, y, z);
+        section.init(pos);
+
+        this.byPosition.put(pos.asLong(), section);
+
+        return section;
+    }
+
+    public void invalidate(int x, int y, int z) {
+        this.byPosition.remove(ChunkSectionPos.asLong(x, y, z));
+    }
+
+    public void release(ClonedChunkSection section) {
+        if (section.releaseReference()) {
+            this.tryReclaim(section);
+        }
+    }
+
+    private ClonedChunkSection allocate() {
+        return new ClonedChunkSection(this, this.world);
+    }
+
+    private void tryReclaim(ClonedChunkSection section) {
+        this.inactivePool.add(section);
+    }
+}

spoorn/sodium-forge

src/main/java/me/jellysquid/mods/sodium/client/render/SodiumWorldRenderer.java
@@ -21,7 +21,7 @@ import me.jellysquid.mods.sodium.client.render.chunk.data.ChunkRenderData;
 import me.jellysquid.mods.sodium.client.render.chunk.format.DefaultModelVertexFormats;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPass;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPassManager;
-import me.jellysquid.mods.sodium.client.render.pipeline.context.GlobalRenderContext;
+import me.jellysquid.mods.sodium.client.render.pipeline.context.ChunkRenderCacheShared;
 import me.jellysquid.mods.sodium.client.util.math.FrustumExtended;
 import me.jellysquid.mods.sodium.client.world.ChunkStatusListener;
 import me.jellysquid.mods.sodium.client.world.ChunkStatusListenerManager;
@@ -118,7 +118,7 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
     private void loadWorld(ClientWorld world) {
         this.world = world;
 
-        GlobalRenderContext.createRenderContext(this.world);
+        ChunkRenderCacheShared.createRenderContext(this.world);
 
         this.initRenderer();
 
@@ -126,7 +126,7 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
     }
 
     private void unloadWorld() {
-        GlobalRenderContext.destroyRenderContext(this.world);
+        ChunkRenderCacheShared.destroyRenderContext(this.world);
 
         if (this.chunkRenderManager != null) {
             this.chunkRenderManager.destroy();
@@ -191,14 +191,14 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
             throw new IllegalStateException("Client instance has no active player entity");
         }
 
-        Vector3d cameraPos = camera.getProjectedView();
+        Vector3d pos = camera.getProjectedView();
 
-        this.chunkRenderManager.setCameraPosition(cameraPos.x, cameraPos.y, cameraPos.z);
+        this.chunkRenderManager.setCameraPosition(pos.x, pos.y, pos.z);
 
         float pitch = camera.getPitch();
         float yaw = camera.getYaw();
 
-        boolean dirty = cameraPos.x != this.lastCameraX || cameraPos.y != this.lastCameraY || cameraPos.z != this.lastCameraZ ||
+        boolean dirty = pos.x != this.lastCameraX || pos.y != this.lastCameraY || pos.z != this.lastCameraZ ||
                 pitch != this.lastCameraPitch || yaw != this.lastCameraYaw;
 
 
@@ -211,9 +211,9 @@ public class SodiumWorldRenderer implements ChunkStatusListener {
             this.chunkRenderManager.markCameraPosChanged();
         }
 
-        this.lastCameraX = cameraPos.x;
-        this.lastCameraY = cameraPos.y;
-        this.lastCameraZ = cameraPos.z;
+        this.lastCameraX = pos.x;
+        this.lastCameraY = pos.y;
+        this.lastCameraZ = pos.z;
         this.lastCameraPitch = pitch;
         this.lastCameraYaw = yaw;
 

src/main/java/me/jellysquid/mods/sodium/client/render/pipeline/context/ChunkRenderCacheLocal.java
@@ -1,0 +1,57 @@
+package me.jellysquid.mods.sodium.client.render.pipeline.context;
+
+import me.jellysquid.mods.sodium.client.model.light.LightPipelineProvider;
+import me.jellysquid.mods.sodium.client.model.light.cache.ArrayLightDataCache;
+import me.jellysquid.mods.sodium.client.model.quad.blender.BiomeColorBlender;
+import me.jellysquid.mods.sodium.client.render.pipeline.BlockRenderer;
+import me.jellysquid.mods.sodium.client.render.pipeline.ChunkRenderCache;
+import me.jellysquid.mods.sodium.client.render.pipeline.FluidRenderer;
+import me.jellysquid.mods.sodium.client.world.WorldSlice;
+import me.jellysquid.mods.sodium.client.world.cloned.ChunkRenderContext;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockModelShapes;
+import net.minecraft.world.World;
+
+public class ChunkRenderCacheLocal extends ChunkRenderCache {
+    private final ArrayLightDataCache lightDataCache;
+
+    private final BlockRenderer blockRenderer;
+    private final FluidRenderer fluidRenderer;
+
+    private final BlockModelShapes blockModels;
+    private final WorldSlice worldSlice;
+
+    public ChunkRenderCacheLocal(Minecraft client, World world) {
+        this.worldSlice = new WorldSlice(world);
+        this.lightDataCache = new ArrayLightDataCache(this.worldSlice);
+
+        LightPipelineProvider lightPipelineProvider = new LightPipelineProvider(this.lightDataCache);
+        BiomeColorBlender biomeColorBlender = this.createBiomeColorBlender();
+
+        this.blockRenderer = new BlockRenderer(client, lightPipelineProvider, biomeColorBlender);
+        this.fluidRenderer = new FluidRenderer(client, lightPipelineProvider, biomeColorBlender);
+
+        this.blockModels = client.getModelManager().getBlockModelShapes();
+    }
+
+    public BlockModelShapes getBlockModels() {
+        return this.blockModels;
+    }
+
+    public BlockRenderer getBlockRenderer() {
+        return this.blockRenderer;
+    }
+
+    public FluidRenderer getFluidRenderer() {
+        return this.fluidRenderer;
+    }
+
+    public void init(ChunkRenderContext context) {
+        this.lightDataCache.reset(context.getOrigin());
+        this.worldSlice.copyData(context);
+    }
+
+    public WorldSlice getWorldSlice() {
+        return this.worldSlice;
+    }
+}

src/main/java/me/jellysquid/mods/sodium/client/world/cloned/ChunkRenderContext.java
@@ -1,0 +1,37 @@
+package me.jellysquid.mods.sodium.client.world.cloned;
+
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.math.SectionPos;
+
+public class ChunkRenderContext {
+    private final SectionPos origin;
+    private final ClonedChunkSection[] sections;
+    private final MutableBoundingBox volume;
+
+    public ChunkRenderContext(SectionPos origin, ClonedChunkSection[] sections, MutableBoundingBox volume) {
+        this.origin = origin;
+        this.sections = sections;
+        this.volume = volume;
+    }
+
+    public ClonedChunkSection[] getSections() {
+        return this.sections;
+    }
+
+    public SectionPos getOrigin() {
+        return this.origin;
+    }
+
+    public MutableBoundingBox getVolume() {
+        return this.volume;
+    }
+
+    public void releaseResources() {
+        for (ClonedChunkSection section : sections) {
+            if (section != null) {
+                section.getBackingCache()
+                        .release(section);
+            }
+        }
+    }
+}

src/main/java/me/jellysquid/mods/sodium/client/world/cloned/ClonedChunkSectionCache.java
@@ -1,0 +1,71 @@
+package me.jellysquid.mods.sodium.client.world.cloned;
+
+import it.unimi.dsi.fastutil.longs.Long2ReferenceMap;
+import it.unimi.dsi.fastutil.longs.Long2ReferenceOpenHashMap;
+import net.minecraft.util.math.SectionPos;
+import net.minecraft.world.World;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ClonedChunkSectionCache {
+    private final World world;
+
+    private final ConcurrentLinkedQueue<ClonedChunkSection> inactivePool = new ConcurrentLinkedQueue<>();
+    private final Long2ReferenceMap<ClonedChunkSection> byPosition = new Long2ReferenceOpenHashMap<>();
+
+    public ClonedChunkSectionCache(World world) {
+        this.world = world;
+    }
+
+    public ClonedChunkSection acquire(int x, int y, int z) {
+        long key = SectionPos.asLong(x, y, z);
+        ClonedChunkSection section = this.byPosition.get(key);
+
+        if (section != null) {
+            this.inactivePool.remove(section);
+        } else {
+            section = this.createSection(x, y, z);
+        }
+
+        section.acquireReference();
+
+        return section;
+    }
+
+    private ClonedChunkSection createSection(int x, int y, int z) {
+        ClonedChunkSection section;
+
+        if (!this.inactivePool.isEmpty()) {
+            section = this.inactivePool.remove();
+
+            this.byPosition.remove(section.getPosition().asLong());
+        } else {
+            section = this.allocate();
+        }
+
+        SectionPos pos = SectionPos.of(x, y, z);
+        section.init(pos);
+
+        this.byPosition.put(pos.asLong(), section);
+
+        return section;
+    }
+
+    public void invalidate(int x, int y, int z) {
+        this.byPosition.remove(SectionPos.asLong(x, y, z));
+    }
+
+    public void release(ClonedChunkSection section) {
+        if (section.releaseReference()) {
+            this.tryReclaim(section);
+        }
+    }
+
+    private ClonedChunkSection allocate() {
+        return new ClonedChunkSection(this, this.world);
+    }
+
+    private void tryReclaim(ClonedChunkSection section) {
+        this.inactivePool.add(section);
+    }
+}