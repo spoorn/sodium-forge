CaffeineMC/sodium-fabric

new: Add version information to F3, closes #598

src/main/java/me/jellysquid/mods/sodium/client/SodiumClientMod.java
@@ -3,19 +3,28 @@ package me.jellysquid.mods.sodium.client;
 import me.jellysquid.mods.sodium.client.gui.SodiumGameOptions;
 import me.jellysquid.mods.sodium.client.util.UnsafeUtil;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
 import java.nio.file.Paths;
 
 public class SodiumClientMod implements ClientModInitializer {
     private static SodiumGameOptions CONFIG;
     private static Logger LOGGER;
 
+    private static String MOD_VERSION;
+
     @Override
     public void onInitializeClient() {
+        ModContainer mod = FabricLoader.getInstance()
+                .getModContainer("sodium")
+                .orElseThrow(NullPointerException::new);
 
+        MOD_VERSION = mod.getMetadata()
+                .getVersion()
+                .getFriendlyString();
     }
 
     public static SodiumGameOptions options() {
@@ -43,5 +52,13 @@ public class SodiumClientMod implements ClientModInitializer {
 
     public static void onConfigChanged(SodiumGameOptions options) {
         UnsafeUtil.setEnabled(options.advanced.useMemoryIntrinsics);
+    }
+
+    public static String getVersion() {
+        if (MOD_VERSION == null) {
+            throw new NullPointerException("Mod version hasn't been populated yet");
+        }
+
+        return MOD_VERSION;
     }
 }

src/main/java/me/jellysquid/mods/sodium/mixin/features/debug/MixinDebugHud.java
@@ -1,14 +1,18 @@
 package me.jellysquid.mods.sodium.mixin.features.debug;
 
+import com.google.common.collect.Lists;
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
 import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderBackend;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.DebugHud;
 import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.lang.management.ManagementFactory;
@@ -22,25 +26,45 @@ public abstract class MixinDebugHud {
         throw new UnsupportedOperationException();
     }
 
-    @Inject(method = "getRightText", at = @At("RETURN"))
-    private void appendRightText(CallbackInfoReturnable<List<String>> cir) {
-        List<String> strings = cir.getReturnValue();
-
-        for (int i = 0; i < strings.size(); i++) {
-            String str = strings.get(i);
-
-            if (str.startsWith("Allocated:")) {
-                strings.add(i + 1, getNativeMemoryString());
-                break;
-            }
-        }
-
+    @Redirect(method = "getRightText", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayList([Ljava/lang/Object;)Ljava/util/ArrayList;"))
+    private ArrayList<String> redirectRightTextEarly(Object[] elements) {
+        ArrayList<String> strings = Lists.newArrayList((String[]) elements);
+        strings.add("");
+        strings.add("Sodium Renderer");
+        strings.add(Formatting.UNDERLINE + getFormattedVersionText());
         strings.add("");
         strings.addAll(getChunkRendererDebugStrings());
 
         if (SodiumClientMod.options().advanced.disableDriverBlacklist) {
             strings.add(Formatting.RED + "(!!) Driver blacklist ignored");
         }
+
+        for (int i = 0; i < strings.size(); i++) {
+            String str = strings.get(i);
+
+            if (str.startsWith("Allocated:")) {
+                strings.add(i + 1, getNativeMemoryString());
+
+                break;
+            }
+        }
+
+        return strings;
+    }
+
+    private static String getFormattedVersionText() {
+        String version = SodiumClientMod.getVersion();
+        Formatting color;
+
+        if (version.endsWith("-dirty")) {
+            color = Formatting.RED;
+        } else if (version.contains("+rev.")) {
+            color = Formatting.LIGHT_PURPLE;
+        } else {
+            color = Formatting.GREEN;
+        }
+
+        return color + version;
     }
 
     private static List<String> getChunkRendererDebugStrings() {

spoorn/sodium-forge

src/main/java/me/jellysquid/mods/sodium/client/SodiumClientMod.java
@@ -3,26 +3,36 @@ package me.jellysquid.mods.sodium.client;
 import me.jellysquid.mods.sodium.client.gui.SodiumGameOptions;
 import me.jellysquid.mods.sodium.client.util.UnsafeUtil;
 import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import net.minecraftforge.fml.ModList;
 
-import java.io.File;
 import java.nio.file.Paths;
 
 @Mod("sodium")
 public class SodiumClientMod {
     private static SodiumGameOptions CONFIG;
     private static Logger LOGGER;
+    private static String MOD_VERSION;
     public static boolean ftbChunksLoaded;
+
     public SodiumClientMod() {
         ftbChunksLoaded = ModList.get().isLoaded("ftbchunks");
         // Register the setup method for modloading
         //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        ModContainer mod = ModList.get()
+                .getModContainerById("sodium")
+                .orElseThrow(NullPointerException::new);
+
+        MOD_VERSION = mod.getModInfo()
+                .getVersion()
+                .getQualifier();
 
         //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
@@ -56,5 +66,13 @@ public class SodiumClientMod {
 
     public static void onConfigChanged(SodiumGameOptions options) {
         UnsafeUtil.setEnabled(options.advanced.useMemoryIntrinsics);
+    }
+
+    public static String getVersion() {
+        if (MOD_VERSION == null) {
+            throw new NullPointerException("Mod version hasn't been populated yet");
+        }
+
+        return MOD_VERSION;
     }
 }

src/main/java/me/jellysquid/mods/sodium/mixin/features/debug/MixinDebugHud.java
@@ -1,5 +1,6 @@
 package me.jellysquid.mods.sodium.mixin.features.debug;
 
+import com.google.common.collect.Lists;
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
 import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderBackend;
@@ -8,8 +9,7 @@ import net.minecraft.util.text.TextFormatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
@@ -22,25 +22,45 @@ public abstract class MixinDebugHud {
         throw new UnsupportedOperationException();
     }
 
-    @Inject(method = "getDebugInfoRight", at = @At("RETURN"))
-    private void appendRightText(CallbackInfoReturnable<List<String>> cir) {
-        List<String> strings = cir.getReturnValue();
-
-        for (int i = 0; i < strings.size(); i++) {
-            String str = strings.get(i);
-
-            if (str.startsWith("Allocated:")) {
-                strings.add(i + 1, getNativeMemoryString());
-                break;
-            }
-        }
-
+    @Redirect(method = "getDebugInfoRight", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayList([Ljava/lang/Object;)Ljava/util/ArrayList;"))
+    private ArrayList<String> redirectRightTextEarly(Object[] elements) {
+        ArrayList<String> strings = Lists.newArrayList((String[]) elements);
+        strings.add("");
+        strings.add("Sodium Renderer");
+        strings.add(TextFormatting.UNDERLINE + getFormattedVersionText());
         strings.add("");
         strings.addAll(getChunkRendererDebugStrings());
 
         if (SodiumClientMod.options().advanced.disableDriverBlacklist) {
             strings.add(TextFormatting.RED + "(!!) Driver blacklist ignored");
         }
+
+        for (int i = 0; i < strings.size(); i++) {
+            String str = strings.get(i);
+
+            if (str.startsWith("Allocated:")) {
+                strings.add(i + 1, getNativeMemoryString());
+
+                break;
+            }
+        }
+
+        return strings;
+    }
+
+    private static String getFormattedVersionText() {
+        String version = SodiumClientMod.getVersion();
+        TextFormatting color;
+
+        if (version.endsWith("-dirty")) {
+            color = TextFormatting.RED;
+        } else if (version.contains("+rev.")) {
+            color = TextFormatting.LIGHT_PURPLE;
+        } else {
+            color = TextFormatting.GREEN;
+        }
+
+        return color + version;
     }
 
     private static List<String> getChunkRendererDebugStrings() {